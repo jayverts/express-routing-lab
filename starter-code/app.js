01ea3c82795c12231321ffcea25648a8cceb4720
jayverts <j.verdolino@gmail.com>
2017-07-20 23:02:46 -0600

    nightly commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,17 @@
 //Did you use npm install to
 //add all these packages
 //to our project?
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 // How do we 'require' the candyRouter file?
-var candyRouter;
+const candyRouter = require('./candyRouter.js');
 
 app.use(bodyParser.json());
-
+app.use('/candies',candyRouter);
 //How do we redirect the /candies path
 //through our candyRouter?
 //Hint: you need app.use
 
-app.listen(3000);
+app.listen(port);
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,17 +1,56 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
-var candies;
+var candies = [
+{id: 1, name: "Chewing Gum", color: "Red"},
+{id: 2, name: "Pez", color: "Green"},
+{id: 3, name: "Marshmallow", color: "Pink"},
+{id: 4, name: "Candy Stick", color: "Blue"}
+];
 
 //What would need to go into candies
 //in order to pass our first test?
 
 router.get('/', function(req,res) {
+	res.json(candies);
 	// What would go here? 
 	// Hint: we want all candies in JSON format
 });
 
 // Fill out the rest of the routes here
+
+router.get('/:id', function(req,res) {
+	//show
+	res.send(candies[req.params.id - 1 ]);
+});
+
+router.get('/new', function(req, res) {
+	//new
+});
+
+router.post('/', function(req,res) {
+	//create
+	candies.push(req.body);
+	res.send(req.body);
+});
+
+router.get('/:id/edit', function(req,res) {
+	//edi
+});
+
+router.put('/:id', function(req,res) {
+	//updates
+	candies[req.params.id - 1 ] = req.body;
+	res.send(req.body);
+
+});
+
+router.delete('/:id', function(req,res) {
+	//delete
+	candies.splice(req.params.id-1,1);
+	res.send({"message":"deleted"});
+
+});
 
 module.exports = router;
